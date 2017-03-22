835f6e55a4f9ec852c97919ea824fc0c91aa6152
Tim White <tim@timwhite.ninja>
2017-03-22 09:58:55 -0700

    init service

---
+++ index.js
@@ -0,0 +1,42 @@
+import { inherits } from 'util';
+
+import { EventEmitter } from 'events';
+
+function WatchtowerService(options) {
+  EventEmitter.call(this);
+  this.node = options.node;
+  this.bus = this.node.openBus();
+}
+inherits(WatchtowerService, EventEmitter);
+
+WatchtowerService.dependencies = ['bitcoind'];
+
+WatchtowerService.prototype.start = function(callback) {
+  this.bus.subscribe('bitcoind/rawtransaction');
+  bus.on('bitcoind/rawtransaction', (transactionHex) => {
+    console.log('GOT RAW TX', transactionHex);
+  });
+
+  console.log('starting service');
+  setImmediate(callback);
+};
+
+WatchtowerService.prototype.stop = function(callback) {
+  console.log('stopping service');
+  this.bus.unsubscribe('bitcoind/rawtransaction');
+  setImmediate(callback);
+};
+
+WatchtowerService.prototype.getAPIMethods = function() {
+  return [];
+};
+
+Service.prototype.getRoutePrefix = function() {
+  return 'watchtower';
+};
+
+WatchtowerService.prototype.getPublishEvents = function() {
+  return [];
+};
+
+module.exports = WatchtowerService;
